205c8c4136c9a6260032e01eccd895dc26932abd
Shengtian Mian <148187254+tianlearner@users.noreply.github.com>
2025-02-24 11:27:02 +0800

    Update train.py

--- code/train.py
+++ code/train.py
@@ -12,6 +12,8 @@ from my_dataset import MyDataSet
 from model import FWNet_ECA_tiny_patch4_window7_224 as create_model
 from utils import read_split_data, train_one_epoch, evaluate
 
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 
 
 def main(args):
@@ -76,12 +78,21 @@ def main(args):
         #weights_dict = torch.load(args.weights, map_location=device)["model"] #torch.save({"model": model.state_dict(), "optimizer": optimizer.state_dict()}, path)
         weights_dict = torch.load(args.weights, map_location=device)
         
-        # del weights_dict["head.weight"] 
-        '''             
-        for k in list(weights_dict.keys()):
-            if "head" in k:
-                del weights_dict[k]
-        '''
+                     
+        # 检查并打印分类头大小
+        if "head.weight" in weights_dict:
+            pre_weights_shape = weights_dict["head.weight"].shape
+            model_head_shape = model.head.weight.shape
+            print(f"预训练模型分类头大小: {pre_weights_shape}")
+            print(f"当前模型分类头大小: {model_head_shape}")
+            
+            # 如果分类头大小不一致，删除预训练权重中的分类头
+            if pre_weights_shape != model_head_shape:
+                print("分类头大小不匹配，删除预训练权重中的分类头...")
+                for k in list(weights_dict.keys()):
+                    if "head" in k:
+                        del weights_dict[k]
+        
         
         print(model.load_state_dict(weights_dict, strict=False))
     
@@ -145,21 +156,21 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=102)
+    parser.add_argument('--num_classes', type=int, default=5013)
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--lr', type=float, default=0.0005)
 
 
     parser.add_argument('--data-path', type=str,
-                        default="")
+                        default="E:\\cartoonface")# 数据集根目录，要求分为train和val两个文件夹，其中每个类别一个文件夹，文件夹名为类别名
 
     # weights_dir
-    parser.add_argument('--weights', type=str, default='', help='the path of weights')
-    parser.add_argument('--weights_dir', type=str, default='', help='weights save dir')
+    parser.add_argument('--weights', type=str, default='F:\\pylearn\\python code\\fwin_transformer-v1\\weights\\imagenet\\tiny\\guanfang-0.809.pth', help='the path of pretrain_weights')
+    parser.add_argument('--weights_dir', type=str, default='F:\\pylearn\\FwNet-ECA-main\\FwNet-ECA-main\\weight', help='weights save dir')
     
     # tensorboard_dir
-    parser.add_argument('--logdir', type=str, default='', help='tensorboard log dir')
+    parser.add_argument('--logdir', type=str, default='F:\\pylearn\\FwNet-ECA-main\\FwNet-ECA-main\\logs', help='tensorboard log dir')
     
 
     parser.add_argument('--freeze-layers', type=bool, default=False, help='if freeze not head layers')
